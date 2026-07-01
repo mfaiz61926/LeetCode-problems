c829bd2ab0017771dc2fa5b545184ca6ca2b4604
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-01 13:27:12 +0530

    Time: 91 ms (76.24%), Space: 192.3 MB (91.07%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& v, int fStart, int fEnd) {
-        int n = v.size();
+    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& v, int fs, int fe) {
         sort(v.begin(), v.end());
+        vector<vector<int>>merge;
+        for(auto &it : v){
+            if(merge.empty() || merge.back()[1] + 1 < it[0]){
+                merge.push_back(it);
+            }
+            else merge.back()[1] = max(merge.back()[1], it[1]);
+        }
 
         vector<vector<int>>ans;
-        if(n == 0) return ans;
-        ans.push_back(v[0]);
-        for(int i = 1; i < n; i++){
-            auto it = v[i];
-            int x = it[0], y = it[1];
-            int a = ans.back()[0], b = ans.back()[1];
-            if(b + 1 < x) ans.push_back(it);
-            else {
-                ans.back()[1] = max(ans.back()[1], y);
+        for(auto &it : merge){
+            int l = it[0], r = it[1];
+            //case 1: interval lies in fs and fe inclusive
+            if(fs <= l && r <= fe) continue;
+            //case 2: out of fs and fe
+            if(l > fe || r < fs){
+                ans.push_back(it);
             }
+            //case: l and r in fs and fe
+            else if(l < fs && fe < r){
+                ans.push_back({l, fs - 1});
+                ans.push_back({fe + 1, r});
+            }
+            //case 4: l < fs
+            else if(l < fs){
+                ans.push_back({l, fs - 1});
+            }
+            else{ // fs <= l
+                ans.push_back({fe + 1, r});
+            }
+
         }
-        bool s = false, e = false;
-        vector<vector<int>>res;
-        for(auto &it : ans){
-            if(it[0] >= fStart && it[1] <= fEnd)continue;
-            else if(it[1] < fStart || it[0] > fEnd){
-                res.push_back(it);
-            }
-            else if(it[0] < fStart && fEnd < it[1]){
-                res.push_back({it[0], fStart - 1});
-                res.push_back({fEnd + 1, it[1]});
-            }
-            else if(it[0] < fStart){
-                res.push_back({it[0], fStart - 1});
-            }
-            else res.push_back({fEnd + 1, it[1]});
-            
-        }
-        return res;
+        return ans;
     }
 };
