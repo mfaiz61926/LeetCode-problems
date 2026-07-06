31927ed997986dfd15434da16e29b6216334a5d5
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-06 10:42:30 +0530

    Time: 6 ms (18.28%), Space: 15.4 MB (19.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int cnt = 0;
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-        vector<int>vis(n, 0);
-        for(int i = 0; i < n; i++){
-            if(vis[i]) continue;
-            for(int j = 0; j < n; j++){
-                if(i != j && !vis[j] && intervals[i][0] <= intervals[j][0] && intervals[j][1] <= intervals[i][1]){
-                    vis[j] = 1;
-                }
+    int removeCoveredIntervals(vector<vector<int>>& range) {
+        int n = range.size();
+        sort(range.begin(), range.end(), [](const vector<int>&a, const vector<int>&b){
+            if(a[0] == b[0]){
+                return a[1] > b[1];
             }
-        }
+            else return a[0] < b[0];
+        });
 
-        for(int i = 0; i < n; i++){
-            if(!vis[i]) cnt++;
+        vector<vector<int>>ans;
+        ans.push_back(range[0]);
+        for(int i = 1; i < n; i++){
+            if(ans.back()[1] >= range[i][1]) continue;
+            else ans.push_back(range[i]);
         }
-        return cnt;
+        for(auto i : ans){
+            for(auto it : i){
+                cout << it << " ";
+            }
+            cout << endl;
+        }
+        return ans.size();
+
     }
 };
