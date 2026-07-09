f1293a749660099870b7a88d91df8bce1c8df25e
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-09 13:43:36 +0530

    Time: 419 ms (13.69%), Space: 402.4 MB (13.69%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int bs(vector<int>&nums, int val){
-        int n = nums.size();
-        int ans = -1; 
+    int bs(vector<int>&v, int val){
+        int n = v.size();
+        int ans = 0;
         int low = 0, high = n - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(nums[mid] <= val){
+            if(v[mid] <= val){
                 ans = mid;
                 low = mid + 1;
             }
@@ -14,56 +14,52 @@ public:
         }
         return ans;
     }
-    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
 
-        vector<pair<int,int>> a;
-        for(int i=0;i<n;i++){
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pair<int,int>>a;
+        for(int i = 0; i < n ; i++){
             a.push_back({nums[i], i});
         }
+
         sort(a.begin(), a.end());
-        // pos[original index] = position in sorted array
-        vector<int> pos(n);
-        vector<int> val(n);
 
-        for(int i=0;i<n;i++){
-            val[i]=a[i].first;
-            pos[a[i].second]=i;
+        vector<int>pos(n), val(n);
+
+        for(int i = 0; i < n; i++){
+            val[i] = a[i].first;
+            pos[a[i].second] = i;
+        }
+        vector<int>reach(n);
+        for(int i = 0; i < n; i++){
+            int idx = bs(val, maxDiff + val[i]);
+            reach[i] = idx;
         }
 
-        // farthest reachable index in one edge
-        vector<int> reach(n);
-        for(int i=0;i<n;i++){
-            // int idx=upper_bound(val.begin(), val.end(), val[i]+maxDiff)-val.begin()-1;
-            int idx = bs(val, val[i] + maxDiff);
-            // if(idx == -1)continue;
-            reach[i]=idx;
-        }
-
-        int LOG = 20;
-        vector<vector<int>> up(n, vector<int>(LOG));
-        for(int i = 0; i < n; i++)
+        int mx_bit = 20;
+        vector<vector<int>>up(n, vector<int>(mx_bit));
+        for(int i = 0; i < n; i++){
             up[i][0] = reach[i];
-
-        for(int j = 1; j < LOG; j++){
-            for(int i = 0; i < n; i++){
-                up[i][j] = up[up[i][j - 1]][j - 1];
+        }
+        for(int i = 1; i < mx_bit; i++){
+            for(int u = 0; u < n; u++){
+                up[u][i] = up[up[u][i - 1]][i - 1];
             }
         }
 
-        vector<int> ans;
-        for(auto &q:queries){
+        vector<int>ans;
+        for(auto &it : queries){
+            int u = it[0], v = it[1];
 
-            int l=pos[q[0]];
-            int r=pos[q[1]];
+            int l = pos[u];
+            int r = pos[v];
 
-            if(l>r) swap(l,r);
-
-            if(l==r){
+            if(l > r) swap(l, r);
+            if(l == r){
                 ans.push_back(0);
                 continue;
             }
 
-            if(reach[l]==l){
+            if(reach[l] == l){
                 ans.push_back(-1);
                 continue;
             }
@@ -71,17 +67,18 @@ public:
             int cur = l;
             int steps = 0;
 
-            for(int j = LOG - 1; j >= 0; j--){
+            for(int j = mx_bit - 1; j >= 0; j--){
                 if(up[cur][j] < r){
                     cur = up[cur][j];
                     steps += (1 << j);
                 }
             }
 
-            if(reach[cur] >= r)
+            if(reach[cur] >= r){
                 ans.push_back(steps + 1);
-            else
-                ans.push_back(-1);
+            }
+            else ans.push_back(-1);
+
         }
 
         return ans;
