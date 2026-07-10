870dda5d5bc7bea272352445786d25eed6a75762
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-10 19:26:35 +0530

    Time: 0 ms (100%), Space: 12.6 MB (31.69%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int dp[501];
-    int f(int i, vector<int>&arr, int n, int k){
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int mx = INT_MIN;
-        int len = 0;
-        int ans = INT_MIN;
+    // int dp[501];
+    // int f(int i, vector<int>&arr, int n, int k){
+    //     if(i == n) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int mx = INT_MIN;
+    //     int len = 0;
+    //     int ans = INT_MIN;
 
-        for(int j = i; j < min(n, i + k); j++){
-            mx = max(mx, arr[j]);
-            len++;
-            ans = max(ans, len * mx + f(j + 1, arr, n, k));
-        }
-        return dp[i] = ans;
-    }
+    //     for(int j = i; j < min(n, i + k); j++){
+    //         mx = max(mx, arr[j]);
+    //         len++;
+    //         ans = max(ans, len * mx + f(j + 1, arr, n, k));
+    //     }
+    //     return dp[i] = ans;
+    // }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        memset(dp, -1, sizeof(dp));
-        return f(0, arr, n, k);
+        // memset(dp, -1, sizeof(dp));
+        // return f(0, arr, n, k);
+        vector<int>dp(n + 1, 0);
+        
+        for(int i = n - 1; i >= 0; i--){
+            int len = 0;
+            int mx = INT_MIN;
+            int ans = INT_MIN;
+            for(int j = i; j < min(n, i + k); j++){
+                len++;
+                mx = max(mx, arr[j]);
+                ans = max(ans, len * mx + dp[j + 1]);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
