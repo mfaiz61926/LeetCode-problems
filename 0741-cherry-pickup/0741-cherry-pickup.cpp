29d4820d62ee5486a7917b57c9525fa83c23e4e4
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-12 22:31:24 +0530

    Time: 37 ms (72.86%), Space: 14.4 MB (86.83%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     int dp[51][51][51];
-
+    int n;
     int solve(vector<vector<int>>& grid, int r1, int c1, int r2) {
-        int n = grid.size();
         int c2 = r1 + c1 - r2;
 
-        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n)
-            return -1000000;
-
-        if (grid[r1][c1] == -1 || grid[r2][c2] == -1)
-            return -1000000;
+        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return INT_MIN;
 
         if (r1 == n - 1 && c1 == n - 1)
             return grid[r1][c1];
 
-        int &ans = dp[r1][c1][r2];
-        if (ans != -1)
-            return ans;
+        if (dp[r1][c1][r2] != -1)
+            return dp[r1][c1][r2];
 
         int cherries = grid[r1][c1];
-        if (r1 != r2 || c1 != c2)
-            cherries += grid[r2][c2];
+        if (r1 != r2 || c1 != c2) cherries += grid[r2][c2];
 
-        ans = cherries + max({
+        int ans = cherries + max({
             solve(grid, r1 + 1, c1, r2 + 1), // down, down
             solve(grid, r1 + 1, c1, r2),     // down, right
             solve(grid, r1, c1 + 1, r2 + 1), // right, down
             solve(grid, r1, c1 + 1, r2)      // right, right
         });
 
-        return ans;
+        return dp[r1][c1][r2] = ans;
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
+        n = grid.size();
         memset(dp, -1, sizeof(dp));
         return max(0, solve(grid, 0, 0, 0));
     }
