829b129bfb3a00872790d4908e9e395f5d74b4b4
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-13 09:56:38 +0530

    Time: 17 ms (27.47%), Space: 11.4 MB (65.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -4,26 +4,24 @@ public:
         int n = s.size(), reqcnt = t.size();
         if(reqcnt > n) return "";
         int start_idx = -1;
-        int l = 0; 
-        int minw = 1e9;
-        map<char,int>mp;
-        for(char ch : t){
-            mp[ch]++;
-        }
+        int mnw = 1e9;
+        map<char, int>mp;
+        for(auto &c : t) mp[c]++;
+
+        int l = 0;
         for(int r = 0; r < n; r++){
-            if(mp[s[r]]>0) reqcnt--;
+            if(mp[s[r]] > 0) reqcnt--;
             mp[s[r]]--;
             while(reqcnt == 0){
-                if(minw > r - l + 1){
-                    minw = r - l + 1;
+                if(mnw > r - l + 1){
+                    mnw = r - l + 1;
                     start_idx = l;
                 }
                 mp[s[l]]++;
-                if(mp[s[l]]>0) reqcnt++;
+                if(mp[s[l]] > 0) reqcnt++;
                 l++;
             }
-
         }
-        return minw==1e9? "":s.substr(start_idx, minw);
+        return mnw == 1e9 ? "" : s.substr(start_idx, mnw);
     }
 };
