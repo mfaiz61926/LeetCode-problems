db25e1cc664667cd58a521f23d586a982de13a98
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-15 22:54:20 +0530

    Time: 1 ms (21.15%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    int g(int a, int b){
+        if(b == 0) return a;
+        return g(b, a % b);
+    }
+
     int gcdOfOddEvenSums(int n) {
         int sumo = 0, sume = 0;
         for(int i = 0; i <= 2*n; i++){
@@ -7,6 +12,6 @@ public:
             else sume += i;
         }
 
-        return gcd(sumo, sume);
+        return g(sumo, sume);
     }
 };
