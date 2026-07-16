50fbe24a573b8eeb5cefebb3437a9ca596d65632
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-16 11:35:34 +0530

    Time: 104 ms (46.95%), Space: 31.4 MB (7.65%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -3,7 +3,7 @@ public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int n = nums1.size();
         int ans = 0;
-        map<int ,int>mp1, mp2;
+        unordered_map<int ,int>mp1, mp2;
        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
