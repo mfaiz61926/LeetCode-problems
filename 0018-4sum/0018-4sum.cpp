9dd864dbaf387b7c22bffcf945d8ff3ffe244597
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-16 11:03:32 +0530

    Time: 19 ms (80.93%), Space: 17.3 MB (99.12%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,10 +4,11 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
-        set<vector<int>>st;
 
         for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1; j < n - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
                 long long need = target - 1LL * nums[i] - 1LL * nums[j];
                 int low = j + 1;
                 int high = n - 1;
@@ -19,18 +20,15 @@ public:
                         high--;
                     }
                     else{
-                        st.insert({nums[i], nums[j], nums[low], nums[high]});
+                        ans.push_back({nums[i], nums[j], nums[low], nums[high]});
+                        while(low < high && nums[low] == nums[low + 1]) low++;
+                        while(low < high && nums[high] == nums[high - 1]) high--;
                         low++;
                         high--;
                     }
                 }
             }
         }
-
-        for(auto &i : st){
-            ans.push_back(i);
-        }
         return ans;
-
     }
 };
