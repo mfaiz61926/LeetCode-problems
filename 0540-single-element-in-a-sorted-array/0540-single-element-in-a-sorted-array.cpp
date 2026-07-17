14595ef2ffbdb06a27d4763afaf0f1e610e4b9cc
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-17 16:09:09 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.26%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,8 +11,8 @@ public:
         while(low <= high){
             int mid = low + (high - low) / 2;
             if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
-            else if((nums[mid] == nums[mid - 1] && (mid - 1) % 2 == 0) || (nums[mid] == nums[mid + 1] && (mid + 1) % 2)) low = mid + 1;
-            else high = mid - 1;
+            else if((nums[mid] == nums[mid - 1] && (mid - 1) % 2 == 0) || (nums[mid] == nums[mid + 1] && (mid + 1) % 2)) low = mid + 1; // if array till mid is correct so target is in right side.
+            else high = mid - 1; //else in left side
         }
         return -1;
     }
