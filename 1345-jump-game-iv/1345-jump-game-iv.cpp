c7e1971d9cc4a0f8edd4526657bbc295e3ed2b64
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-07-27 18:32:59 +0530

    Time: 105 ms (46.16%), Space: 80.4 MB (47.3%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+
+        map<int,vector<int>>mp;
+        for(int i = 0; i < n; i++){
+            mp[arr[i]].push_back(i);
+        }
+
+        queue<pair<int,int>>q;
+        // priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>>q;
+        vector<int>vis(n, 0);
+        q.push({0, 0});
+        vis[0] = 1;
+
+        while(!q.empty()){
+            int sz = q.size();
+            for(int k = 0; k < sz; k++){
+
+                auto it = q.front();
+                q.pop();
+
+                int level = it.first;
+                int i = it.second;
+
+                if(i == n - 1) return level;
+                if(i + 1 < n && !vis[i + 1]){
+                    q.push({level + 1, i + 1});
+                    vis[i + 1] = 1;
+                }
+                if(i - 1 >= 0 && !vis[i - 1]){
+                    q.push({level + 1, i - 1});
+                    vis[i - 1] = 1;
+                }
+                for(int idx : mp[arr[i]]){
+                    if(!vis[idx]){
+                        q.push({level + 1, idx});
+                        vis[idx] = 1;
+                    }
+                }
+                mp[arr[i]].clear();
+            }
+        }
+        return -1;
+    }
+};
