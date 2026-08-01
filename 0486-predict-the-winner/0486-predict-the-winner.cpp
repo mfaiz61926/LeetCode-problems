4bf48d9b32516e7b5b98fc8e0dc1a628f2f69765
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-08-01 10:34:45 +0530

    Time: 2 ms (43.58%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -17,7 +17,22 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         n = nums.size();
         // memset(dp, INT_MIN, sizeof(dp));
-        dp.assign(n, vector<int>(n, INT_MIN));
-        return f(0, n - 1, nums) >= 0;
+        dp.assign(n, vector<int>(n));
+        // return f(0, n - 1, nums) >= 0;
+
+        for(int i = 0; i < n; i++) dp[i][i] = nums[i];
+
+        for(int len = 2; len <= n; len++){
+            for(int i = 0; i + len - 1 < n; i++){
+                int j = i + len - 1;
+
+                int left = nums[i] - dp[i + 1][j];
+                int right = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = max(left, right);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
     }
 };
