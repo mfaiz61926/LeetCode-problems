ba86040ae53ded87c03ddf0a4e2a9820ff4d416e
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-08-01 10:41:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.24%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string winningPlayer(int x, int y) {
+        int i = 1;
+        while(true){
+            if(x < 1 || y < 4) break;
+            x--, y -= 4;
+            i++;
+        }
+        return i&1 ? "Bob" : "Alice";
+    }
+};
