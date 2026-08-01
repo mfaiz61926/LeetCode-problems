7dfbf2294316e4b10b08c2a96d52b57859ca31ef
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-08-01 10:28:49 +0530

    Time: 0 ms (100%), Space: 10.2 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int n;
+    vector<vector<int>>dp;
     int f(int i , int j, vector<int>&a){
         if(i == j) return a[i];
 
         //p1 takes left
+        if(dp[i][j] != INT_MIN) return dp[i][j];
+
         int left = a[i] - f(i + 1, j, a);
         int right = a[j] - f(i, j - 1, a);
 
-        return max(left, right);
+        return dp[i][j] = max(left, right);
     }
+
     bool predictTheWinner(vector<int>& nums) {
         n = nums.size();
+        // memset(dp, INT_MIN, sizeof(dp));
+        dp.assign(n, vector<int>(n, INT_MIN));
         return f(0, n - 1, nums) >= 0;
     }
 };
