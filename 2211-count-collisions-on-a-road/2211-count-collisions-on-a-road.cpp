e47df8c527b641ac8c56112530c809c2e70a07bd
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-08-23 10:12:03 +0530

    Time: 3 ms (94.16%), Space: 19 MB (95.83%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,39 +1,39 @@
-class SolutionStack {
-public:
-    int countCollisions(string directions) {
-        int n = directions.size();
-        stack<char>st;
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            char c = directions[i];
-            if(st.empty() && (c == 'S' || c == 'R')) st.push(c);
+// class SolutionStack {
+// public:
+//     int countCollisions(string directions) {
+//         int n = directions.size();
+//         stack<char>st;
+//         int cnt = 0;
+//         for(int i = 0; i < n; i++){
+//             char c = directions[i];
+//             if(st.empty() && (c == 'S' || c == 'R')) st.push(c);
 
-            else if(!st.empty() && c == 'L' && st.top() == 'R'){
-                cnt += 2;
-                st.pop();
-                while(!st.empty() && st.top() == 'R'){
-                    cnt++;
-                    st.pop();
-                }
-                st.push('S');
-            }
-            else if(!st.empty() && c == 'L' && st.top() == 'S'){
-                cnt += 1;
-            }
-            else if(c == 'R'){
-                 st.push(c);
-            }
-            else if(!st.empty() && c == 'S' && st.top() == 'R'){
-                while(!st.empty() && st.top() == 'R'){
-                    cnt++;
-                    st.pop();
-                }
-                st.push('S');
-            }
-        }
-        return cnt;
-    }
-};
+//             else if(!st.empty() && c == 'L' && st.top() == 'R'){
+//                 cnt += 2;
+//                 st.pop();
+//                 while(!st.empty() && st.top() == 'R'){
+//                     cnt++;
+//                     st.pop();
+//                 }
+//                 st.push('S');
+//             }
+//             else if(!st.empty() && c == 'L' && st.top() == 'S'){
+//                 cnt += 1;
+//             }
+//             else if(c == 'R'){
+//                  st.push(c);
+//             }
+//             else if(!st.empty() && c == 'S' && st.top() == 'R'){
+//                 while(!st.empty() && st.top() == 'R'){
+//                     cnt++;
+//                     st.pop();
+//                 }
+//                 st.push('S');
+//             }
+//         }
+//         return cnt;
+//     }
+// };
 
 class Solution {
 public:
