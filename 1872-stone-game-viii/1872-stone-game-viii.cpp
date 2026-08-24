c693350d4c7649c9f97683a0391dad739fbc49d3
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-08-24 21:54:16 +0530

    Time: 85 ms (97.36%), Space: 91.8 MB (65.59%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// public:
+//     int stoneGameVIII(vector<int>& stones) {
+//         int n = stones.size();
+//         vector<int>pref(n);
+//         pref[0] = stones[0];
+//         for(int i = 1; i < n; i++){
+//             pref[i] = pref[i - 1] + stones[i];
+//         }
+
+//         vector<int>dp(n); // dp[i] = number of removed stones
+//         dp[n - 1] = pref[n - 1];
+
+//         for(int i = n - 2; i >= 1; i--){
+//             dp[i] = max(pref[i] - dp[i + 1], dp[i + 1]);
+//         }
+//         return dp[1];
+//     }
+// };
+
 class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
@@ -8,12 +28,11 @@ public:
             pref[i] = pref[i - 1] + stones[i];
         }
 
-        vector<int>dp(n); // dp[i] = number of removed stones
-        dp[n - 1] = pref[n - 1];
-
+        int dp = pref[n - 1];
         for(int i = n - 2; i >= 1; i--){
-            dp[i] = max(pref[i] - dp[i + 1], dp[i + 1]);
+            dp = max(pref[i] - dp, dp);
         }
-        return dp[1];
+
+        return dp;
     }
 };
