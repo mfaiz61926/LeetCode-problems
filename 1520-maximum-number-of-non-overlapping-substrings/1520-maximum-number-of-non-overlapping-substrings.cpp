2eb01395d9951a4632c04766ae3ad31bd1cace55
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2026-09-18 07:45:18 +0530

    Time: 416 ms (13.52%), Space: 320.6 MB (5.04%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,146 @@
+class Solution {
+private:
+    void dfs(int node, vector<int>& vis, vector<int> adj[],
+             stack<int>& st) {
+        vis[node] = 1;
+
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it, vis, adj, st);
+            }
+        }
+
+        st.push(node);
+    }
+
+    void dfs3(int node, vector<int>& comp,
+              vector<int>& sccId,
+              int id,
+              vector<int> adjT[]) {
+
+        sccId[node] = id;
+        comp.push_back(node);
+
+        for (auto it : adjT[node]) {
+            if (sccId[it] == -1) {
+                dfs3(it, comp, sccId, id, adjT);
+            }
+        }
+    }
+
+public:
+    vector<string> maxNumOfSubstrings(string S) {
+
+        vector<int> mins(26, INT_MAX);
+        vector<int> maxs(26, -1);
+        vector<int> exists(26, 0);
+
+        int n = S.size();
+
+        vector<vector<int>> prefix(n + 1, vector<int>(26, 0));
+
+        for (int i = 0; i < n; i++) {
+
+            prefix[i + 1] = prefix[i];
+            prefix[i + 1][S[i] - 'a']++;
+
+            mins[S[i] - 'a'] = min(mins[S[i] - 'a'], i);
+            maxs[S[i] - 'a'] = max(maxs[S[i] - 'a'], i);
+
+            exists[S[i] - 'a'] = 1;
+        }
+
+        vector<int> adj[26];
+
+        // Build graph
+        for (int i = 0; i < 26; i++) {
+
+            if (!exists[i]) continue;
+
+            for (int j = 0; j < 26; j++) {
+
+                if (prefix[maxs[i] + 1][j] - prefix[mins[i]][j] > 0) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        // Kosaraju step 1
+        vector<int> vis(26, 0);
+        stack<int> st;
+
+        for (int i = 0; i < 26; i++) {
+            if (exists[i] && !vis[i]) {
+                dfs(i, vis, adj, st);
+            }
+        }
+
+        // Transpose graph
+        vector<int> adjT[26];
+
+        for (int i = 0; i < 26; i++) {
+            for (auto it : adj[i]) {
+                adjT[it].push_back(i);
+            }
+        }
+
+        // Kosaraju step 2
+        vector<int> sccId(26, -1);
+        vector<vector<int>> comps;
+
+        int batch = 0;
+
+        while (!st.empty()) {
+
+            int node = st.top();
+            st.pop();
+
+            if (sccId[node] == -1) {
+
+                vector<int> comp;
+                dfs3(node, comp, sccId, batch, adjT);
+
+                comps.push_back(comp);
+                batch++;
+            }
+        }
+
+        // Out-degree of SCC DAG
+        vector<int> degree(batch, 0);
+
+        for (int u = 0; u < 26; u++) {
+
+            if (!exists[u]) continue;
+
+            for (auto v : adj[u]) {
+
+                if (sccId[u] != sccId[v]) {
+                    degree[sccId[u]]++;
+                }
+            }
+        }
+
+        vector<string> ans;
+
+        for (int id = batch - 1; id >= 0; id--) {
+
+            if (degree[id] == 0) {
+
+                int L = INT_MAX;
+                int R = -1;
+
+                for (int ch = 0; ch < 26; ch++) {
+
+                    if (sccId[ch] == id) {
+                        L = min(L, mins[ch]);
+                        R = max(R, maxs[ch]);
+                    }
+                }
+
+                ans.push_back(S.substr(L, R - L + 1));
+            }
+        }
+
+        return ans;
+    }
+};
